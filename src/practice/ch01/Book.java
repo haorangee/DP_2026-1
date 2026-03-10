3ae916800c5fd5406b9652dcbeac0bde7ba48377
Haneul Lee <duxali78@duksung.ac.kr>
2026-03-10 11:46:47 +0900

    3월10일 수업종료(02반)

---
+++ src/practice/ch01/Book.java
@@ -0,0 +1,13 @@
+package practice.ch01; //작성하는 패키지가 어디에 속하는지 (폴더명과 같아야 함)
+
+public class Book {
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/practice/ch01/Main.java
@@ -0,0 +1,8 @@
+package practice.ch01;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book(name:"Java Programming");
+        System.out.println("Book Name: " + book.getName());
+    }
+}
