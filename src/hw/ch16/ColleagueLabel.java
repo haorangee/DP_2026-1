9335172facc26502ac2bd95a2f205a4c1bbccd85
Haneul Lee <duxali78@duksung.ac.kr>
2026-05-17 02:47:56 +0900

    16장 과제 중

---
+++ src/hw/ch16/ColleagueLabel.java
@@ -0,0 +1,32 @@
+package hw.ch16;
+
+import java.awt.Color;
+import java.awt.Label;  
+
+public class ColleagueLabel extends Label implements Colleague {
+    private Mediator mediator;
+
+    public ColleagueLabel(String caption) {
+        super(caption);
+    }
+
+    // Mediator를 설정한다 
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    // Mediator에서 활성/비활성을 지시한다
+    @Override
+    public void setColleagueEnabled(boolean enabled) {
+        if (enabled) {
+            setText("로그인 가능");
+            setForeground(Color.blue);
+        } else {
+            setText("로그인 불가");
+            setForeground(Color.gray);
+        }
+        setEnabled(enabled);
+    }
+    
+}
